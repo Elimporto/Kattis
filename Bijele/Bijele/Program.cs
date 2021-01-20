3af8343a5bf84ac5f13f0461c5d756b14e405c6a
elimporto <simon.stancovich@gmail.com>
2021-01-20 19:17:50 +0100

    update

---
+++ Bijele/Bijele/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace Bijele
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * King: 1
+             * Queen: 1
+             * Rooks: 2
+             * Bishops: 2
+             * Knights: 2
+             * Pawns: 8
+             */
+
+            int[] comparable = { 1, 1, 2, 2, 2, 8 };
+            string input = Console.ReadLine();
+            string[]arr =input.Split(" ");
+            string output = "";
+            for(int i=0; i < arr.Length; i++)
+            {
+                int diff = 0;
+                int temp = int.Parse(arr[i].ToString());
+                if (i < arr.Length - 1)
+                {
+                    if (temp == comparable[i])
+                    {
+                        output +="0 ";
+                    }
+                    else if (temp != comparable[i])
+                    {
+                        while (temp > comparable[i])
+                        {
+                            temp--;
+                            diff--;
+                        }
+                        while (temp < comparable[i])
+                        {
+                            temp++;
+                            diff++;
+                        }
+                        output += diff + " ";
+                    }
+                }
+                else
+                {
+                    if (temp == comparable[i])
+                    {
+                        output += "0";
+                    }
+                    else if (temp != comparable[i])
+                    {
+                        while (temp > comparable[i])
+                        {
+                            temp--;
+                            diff--;
+                        }
+                        while (temp < comparable[i])
+                        {
+                            temp++;
+                            diff++;
+                        }
+                        output += diff;
+                    }
+                }
+
+            }
+            Console.WriteLine(output);
+        }
+
+    }
+}
---
+++ GrassSeedInc/GrassSeedInc/GrassSeedInc/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace GrassSeedInc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float price = float.Parse(Console.ReadLine());
+            int nrOfLawns = int.Parse(Console.ReadLine());
+            float totalSqrm = 0;
+            int n = 0;
+            while (n < nrOfLawns)
+            {
+                bool firstcont = true;
+                string n1 = "";
+                string n2 = "";
+                string str = Console.ReadLine();
+                for(int i = 0; i < str.Length; i++)
+                {
+                    if(str[i]!=' ' && firstcont == true)
+                    {
+                        n1 += str[i];
+                        continue;
+                    }
+                    else if(str[i]==' ')
+                    {
+                        firstcont = false;
+                        i++;
+                    }
+                    if (firstcont == false)
+                    {
+                        n2 += str[i];
+                    }
+                }
+                float nr1 = float.Parse(n1);
+                float nr2 = float.Parse(n2);
+                totalSqrm += nr1 * nr2;
+                n++;
+            }
+            Console.WriteLine(totalSqrm * price);
+        }
+    }
+}
