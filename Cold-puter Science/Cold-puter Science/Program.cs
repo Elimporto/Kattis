1a383ab6f6220efa4a22c335082d892ad9df1c64
elimporto <simon.stancovich@gmail.com>
2021-02-11 20:03:26 +0100

    C#

---
+++ Cold-puter Science/Cold-puter Science/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cold_puter_Science
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int temperaturesCollected);
+            string[] temperatures = Console.ReadLine().Split(" ");
+            Console.WriteLine(HowManyTempsAreUnderZero(temperatures));
+        }
+        static int HowManyTempsAreUnderZero(string[] temperatures)
+        {
+            int numberOfTemperaturesUnderZero = 0;
+            for(int i = 0; i < temperatures.Length; i++)
+            {
+            int.TryParse(temperatures[i], out int temperature);
+                if (temperature < 0)
+                {
+                    numberOfTemperaturesUnderZero++;
+                }
+            }
+            return numberOfTemperaturesUnderZero;
+        }
+    }
+}
