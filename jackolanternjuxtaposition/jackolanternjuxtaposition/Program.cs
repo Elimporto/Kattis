bead4793d1857e3e2057b7084d0294a143ded3b4
elimporto <simon.stancovich@gmail.com>
2021-03-14 22:03:39 +0100

    C#

---
+++ jackolanternjuxtaposition/jackolanternjuxtaposition/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace jackolanternjuxtaposition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(nrOfDesigns(Console.ReadLine()));
+        }
+
+        static int nrOfDesigns(string input)
+        {
+            string[] tempHolder= input.Split(" ");
+            int.TryParse(tempHolder[0], out int nrEyeDesigns);
+            int.TryParse(tempHolder[1], out int nrNoseDesigns);
+            int.TryParse(tempHolder[2], out int nrMouthDesigns);
+            return nrEyeDesigns * nrNoseDesigns * nrMouthDesigns;
+        }
+    }
+}
