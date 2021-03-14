d9a12bb18b491f268cf275ddd0fff67f518c7d97
elimporto <simon.stancovich@gmail.com>
2021-03-14 22:20:48 +0100

    C#

---
+++ greetings2/greetings2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace greetings2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(greetDoubleE(countNrOfEs(Console.ReadLine())));
+        }
+
+        static int countNrOfEs(string input)
+        {
+            int amountOfEs = 0;
+            foreach (char letter in input)
+            {
+                if (letter != 'e')
+                {
+                    continue;
+                }
+
+                amountOfEs++;
+
+            }
+
+            return amountOfEs;
+        }
+
+        static string greetDoubleE(int input)
+        {
+            string e = "";
+            input *= 2;
+            for (int i = 0; i < input; i++)
+            {
+                e += "e";
+            }
+
+            string hey = $"h{e}y";
+            return hey;
+        }
+    }
+}
