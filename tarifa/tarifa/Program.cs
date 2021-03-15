64b3c7833c66d4872ef9eef82a5687d636b171c0
elimporto <simon.stancovich@gmail.com>
2021-03-15 11:06:02 +0100

    C#

---
+++ tarifa/tarifa/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace tarifa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int X);
+            int.TryParse(Console.ReadLine(), out int N);
+            Console.WriteLine(Tarifa(X,N));
+        }
+        static int Tarifa(int X, int N)
+        {
+            int output = X * (N+1);
+            for(int i = 0; i < N; i++)
+            {
+                int.TryParse(Console.ReadLine(), out int P);
+                output -= P;
+            }
+
+            return output;
+        }
+    }
+}
---
+++ zamka/zamka/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace zamka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int N);
+            int.TryParse(Console.ReadLine(), out int D);
+            int.TryParse(Console.ReadLine(), out int X);
+
+        }
+
+        static int findWhatSumEqualsX(int input, int x)
+        {
+            for(int i = 0; )
+        }
+    }
+}
