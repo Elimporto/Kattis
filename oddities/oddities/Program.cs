275b4d6bc34608459788f6c7752a7c269240c490
elimporto <simon.stancovich@gmail.com>
2021-03-23 19:23:59 +0100

    C#

---
+++ oddities/oddities/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace oddities
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int testCases);
+            for (int i = 0; i < testCases; i++)
+            {
+                int.TryParse(Console.ReadLine(), out int input);
+                Console.WriteLine(isItEvenOrOdd(input));
+            }
+
+        }
+
+        static string isItEvenOrOdd(int input)
+        {
+            string output = "";
+            if (input % 2 == 0)
+            {
+                output = $"{input} is even";
+            }
+            else
+            {
+                output = $"{input} is odd";
+            }
+
+            return output;
+        }
+
+    }
+}
