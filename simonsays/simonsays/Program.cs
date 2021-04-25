0e0000bfcb3c04e3fa83db267262afb14b2e9c28
elimporto <simon.stancovich@gmail.com>
2021-04-25 17:12:45 +0200

    C#

---
+++ simonsays/simonsays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace simonsays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nrOfInput = int.Parse(Console.ReadLine());
+            for(int i = 0; i < nrOfInput; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ");
+                if(input[0] == "Simon" && input[1] == "says")
+                {
+                    for (int b = 2; b < input.Length; b++)
+                    {
+                        Console.Write($" {input[b]}");
+                    }
+                }
+                else
+                {
+                    continue;
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
